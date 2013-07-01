b51c296baedb6490d115214aff1d1cb024a04781
adoublegulp <christopher.d.yost@gmail.com>
2013-07-01 14:50:31 -0500

    Revert "Revert "our old stuff""
    This reverts commit 4706c84d57e382326e0351802b6809a44999b450.

---
+++ AIPlayer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+
+public class AIPlayer : Player {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public override void TurnUpdate ()
+	{
+		transform.renderer.material.color = Color.red;
+		if (Vector3.Distance(moveDestination, transform.position) <= moveDistance) 
+			{
+			if (Vector3.Distance(moveDestination, transform.position) > 0.1f)
+				{
+				transform.position += (moveDestination - transform.position).normalized * moveSpeed * Time.deltaTime;
+			
+				if (Vector3.Distance(moveDestination, transform.position) <= 0.1f) 
+					{
+					transform.position = moveDestination;
+					transform.renderer.material.color = Color.white;
+					generalManager.instance.nextTurn();
+					}
+				}
+			}
+			
+		base.TurnUpdate ();
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public Vector3 moveDestination;
+	public float moveSpeed = 10.0f;
+	public float moveDistance = 10f;
+	
+	void Awake () {
+		moveDestination = transform.position;
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public virtual void TurnUpdate () {
+		
+	}
+}
